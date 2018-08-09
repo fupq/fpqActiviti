905e47b1991ad28242ac998beb1ec2ca8af78751
fupq <850779566@qq.com>
2018-08-10 02:26:37 +0800

    查询activiti内置的25张表的sql语句

---
+++ sql/02_select25TableCount.sql
@@ -0,0 +1,55 @@
+#查询25张表的记录数
+select count(*) from fpqactiviti.act_evt_log;
+select count(*) from fpqactiviti.act_ge_bytearray;
+select count(*) from fpqactiviti.act_ge_property;
+select count(*) from fpqactiviti.act_hi_actinst;
+select count(*) from fpqactiviti.act_hi_attachment;
+select count(*) from fpqactiviti.act_hi_comment;
+select count(*) from fpqactiviti.act_hi_detail;
+select count(*) from fpqactiviti.act_hi_identitylink;
+select count(*) from fpqactiviti.act_hi_procinst;
+select count(*) from fpqactiviti.act_hi_taskinst;
+select count(*) from fpqactiviti.act_hi_varinst;
+select count(*) from fpqactiviti.act_id_group;
+select count(*) from fpqactiviti.act_id_info;
+select count(*) from fpqactiviti.act_id_membership;
+select count(*) from fpqactiviti.act_id_user;
+select count(*) from fpqactiviti.act_procdef_info;
+select count(*) from fpqactiviti.act_re_deployment;
+select count(*) from fpqactiviti.act_re_model;
+select count(*) from fpqactiviti.act_re_procdef;
+select count(*) from fpqactiviti.act_ru_event_subscr;
+select count(*) from fpqactiviti.act_ru_execution;
+select count(*) from fpqactiviti.act_ru_identitylink;
+select count(*) from fpqactiviti.act_ru_job;
+select count(*) from fpqactiviti.act_ru_task;
+select count(*) from fpqactiviti.act_ru_variable;
+
+
+
+#查询25张表的记录
+select * from fpqactiviti.act_evt_log;
+select * from fpqactiviti.act_ge_bytearray;
+select * from fpqactiviti.act_ge_property;
+select * from fpqactiviti.act_hi_actinst;
+select * from fpqactiviti.act_hi_attachment;
+select * from fpqactiviti.act_hi_comment;
+select * from fpqactiviti.act_hi_detail;
+select * from fpqactiviti.act_hi_identitylink;
+select * from fpqactiviti.act_hi_procinst;
+select * from fpqactiviti.act_hi_taskinst;
+select * from fpqactiviti.act_hi_varinst;
+select * from fpqactiviti.act_id_group;
+select * from fpqactiviti.act_id_info;
+select * from fpqactiviti.act_id_membership;
+select * from fpqactiviti.act_id_user;
+select * from fpqactiviti.act_procdef_info;
+select * from fpqactiviti.act_re_deployment;
+select * from fpqactiviti.act_re_model;
+select * from fpqactiviti.act_re_procdef;
+select * from fpqactiviti.act_ru_event_subscr;
+select * from fpqactiviti.act_ru_execution;
+select * from fpqactiviti.act_ru_identitylink;
+select * from fpqactiviti.act_ru_job;
+select * from fpqactiviti.act_ru_task;
+select * from fpqactiviti.act_ru_variable;

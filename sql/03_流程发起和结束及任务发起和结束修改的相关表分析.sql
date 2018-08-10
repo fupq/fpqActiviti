4708efb9a9cdef0018fd99e36503679492f106d9
fupq <850779566@qq.com>
2018-08-11 00:29:33 +0800

    更新表说明

--- sql/03_流程发起和结束及任务发起和结束修改的相关表分析.sql
+++ sql/03_流程发起和结束及任务发起和结束修改的相关表分析.sql
@@ -7,22 +7,24 @@ select * from fpqactiviti.act_ge_bytearray;
 select * from fpqactiviti.act_re_deployment;
 #ACT_RE_PROCDEF（业务流程定义数据表：解析表）:解析成功了，在该表保存一条记录。
 select * from fpqactiviti.act_re_procdef;
+#系统配置表:dbid的取值会变化，记录指针
+select * from fpqactiviti.act_ge_property;
 
 ################################:启动流程实例相关表
-#ACT_HI_ACTINST（历史节点表）：历史活动信息。这里记录流程流转过的所有节点，与HI_TASKINST不同的是，taskinst只记录usertask内容。
-select * from fpqactiviti.act_hi_actinst;
-#ACT_HI_IDENTITYLINK （历史流程人员表）：任务参与者数据表。主要存储历史节点参与者的信息。
-select * from fpqactiviti.act_hi_identitylink;
-#ACT_HI_PROCINST（历史流程实例信息）核心表：历史的流程实例
-select * from fpqactiviti.act_hi_procinst;
-#ACT_HI_TASKINST（历史任务流程实例信息）核心表：历史的任务实例
-select * from fpqactiviti.act_hi_taskinst;
-#ACT_RU_EXECUTION（运行时流程执行实例）：我的代办任务查询表
+#ACT_RU_EXECUTION（流程实例运行时 执行对象表）：我的代办任务查询表
 select * from fpqactiviti.act_ru_execution;
-#ACT_RU_IDENTITYLINK（运行时用户关系信息，身份联系）：主要存储当前节点参与者的信息,任务参与者数据表。
+#ACT_RU_IDENTITYLINK（流程实例运行时 身份联系表）：主要存储当前节点参与者的信息,任务参与者数据表。
 select * from fpqactiviti.act_ru_identitylink;
-#ACT_RU_TASK(运行时任务数据表)：（执行中实时任务）代办任务查询表，运行时任务
+#ACT_RU_TASK(流程实例运行时 用户任务表)：（执行中实时任务）代办任务查询表，运行时任务
 select * from fpqactiviti.act_ru_task;
+#ACT_HI_ACTINST（活动节点历史表）：记录流程流转过的所有节点，与HI_TASKINST不同的是，act_hi_taskinst只记录usertask内容。
+select * from fpqactiviti.act_hi_actinst;
+#ACT_HI_IDENTITYLINK （历史身份联系表）：任务参与者数据表。主要存储历史节点参与者的信息。
+select * from fpqactiviti.act_hi_identitylink;
+#ACT_HI_PROCINST（历史流程实例表）核心表：历史的流程实例
+select * from fpqactiviti.act_hi_procinst;
+#ACT_HI_TASKINST（历史任务表）核心表：历史的任务实例
+select * from fpqactiviti.act_hi_taskinst;
 
 
 ################################:结束流程中某一个任务修改的相关表
@@ -45,21 +47,3 @@ select * from fpqactiviti.act_ru_identitylink;
 #ACT_RU_TASK(运行时任务数据表)：（执行中实时任务）代办任务查询表，运行时任务
 select * from fpqactiviti.act_ru_task;
 
-
-
-#整个流程结束都为修改的表
-select * from fpqactiviti.act_evt_log;
-select * from fpqactiviti.act_ge_property;
-select * from fpqactiviti.act_hi_attachment;
-select * from fpqactiviti.act_hi_comment;
-select * from fpqactiviti.act_hi_detail;
-select * from fpqactiviti.act_hi_varinst;
-select * from fpqactiviti.act_id_group;
-select * from fpqactiviti.act_id_info;
-select * from fpqactiviti.act_id_membership;
-select * from fpqactiviti.act_id_user;
-select * from fpqactiviti.act_procdef_info;
-select * from fpqactiviti.act_re_model;
-select * from fpqactiviti.act_ru_event_subscr;
-select * from fpqactiviti.act_ru_job;
-select * from fpqactiviti.act_ru_variable;
